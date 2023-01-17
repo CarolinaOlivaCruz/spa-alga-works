924f5d04167623f8cef65bb46fd5b9b154f93e48
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-01-17 11:58:33 -0300

    feat: reset css

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,9 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Lato:wght@300;400;700;900&display=swap');
+
 * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: "Lato", sans-serif;
   }
 `;
